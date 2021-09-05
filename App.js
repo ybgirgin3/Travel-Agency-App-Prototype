68e49fd1322a20275be6a7d4bad131ccab5ce77c
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2021-09-06 01:30:27 +0300

    prototype done

--- App.js
+++ App.js
@@ -2,12 +2,93 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import DetailScreen from './Screens/DetailScreen';
+import HomeScreen from './Screens/HomeScreen';
+import colors from './assets/colors/colors'
+import LikedScreen from './Screens/LikedScreen';
+import ProfileScreen from './Screens/ProfileScreen';
+import Entypo from 'react-native-vector-icons/Entypo';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        style: styles.tabBar,
+        activeTintColor: colors.orange,
+        inactiveTintColor: colors.gray,
+        showLabel: false,
+        
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Entypo name="home" size={32} color={color} />
+          ),
+          headerShown: false
+        }}
+      />
+      <Tab.Screen
+        name="Liked"
+        component={LikedScreen}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Entypo name="heart" size={32} color={color} />
+            
+          ),
+          headerShown: false
+
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcons name="account" size={32} color={color} />
+          ),
+          headerShown: false
+
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="TabNavigator"
+        component={TabNavigator}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen 
+        name="Home"  
+        component={HomeScreen}
+        options={{headerShown: false}}
+
+      />
+      <Stack.Screen
+        name="Details"
+        component={DetailScreen}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
---
+++ Screens/LikedScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const LikedScreen = () => {
+    return (
+        <View>
+            <Text>liked</Text>
+        </View>
+    )
+}
+
+export default LikedScreen
+
+const styles = StyleSheet.create({})
